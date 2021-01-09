671a47177491f80e6c083cb414cff18a5ce69672
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2021-01-09 22:00:18 +0100

    Improve mock (#3)

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/bool64/stats
 go 1.13
 
 require (
-	github.com/bool64/dev v0.1.8
+	github.com/bool64/dev v0.1.12
 	github.com/kr/pretty v0.2.0 // indirect
-	github.com/prometheus/client_golang v1.7.1
+	github.com/prometheus/client_golang v1.9.0
 	github.com/stretchr/testify v1.5.1
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	golang.org/x/sys v0.0.0-20210108172913-0df2131ae363 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- mock.go
+++ mock.go
@@ -2,53 +2,111 @@ package stats
 
 import (
 	"context"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 )
 
 // TrackerMock can collect stats for tests with labels ignored.
 type TrackerMock struct {
-	mu     sync.Mutex
-	values map[string]float64
+	mu            sync.Mutex
+	values        map[string]float64
+	labeledValues map[string]float64
+}
+
+var escaper = strings.NewReplacer("\n", `\\n`, `\`, `\\`, `"`, `\"`)
+
+func labelsString(labels []string) string {
+	if len(labels) == 0 {
+		return ""
+	}
+
+	isKey := true
+	key := ""
+
+	if len(labels)%2 != 0 {
+		panic("malformed pairs")
+	}
+
+	type kv struct {
+		k, v string
+	}
+
+	lb := make([]kv, 0, len(labels)/2)
+
+	for _, l := range labels {
+		if isKey {
+			if l == "" {
+				panic("empty key received in labels")
+			}
+
+			key = l
+			isKey = false
+		} else {
+			lb = append(lb, kv{k: key, v: l})
+			isKey = true
+		}
+	}
+
+	sort.Slice(lb, func(i, j int) bool {
+		return lb[i].k < lb[j].v
+	})
+
+	res := ""
+	for _, i := range lb {
+		res += i.k + `="` + escaper.Replace(i.v) + `",`
+	}
+
+	return res[0 : len(res)-1]
 }
 
 // Add collects metric increment.
-func (t *TrackerMock) Add(_ context.Context, name string, increment float64, _ ...string) {
+func (t *TrackerMock) Add(_ context.Context, name string, increment float64, labels ...string) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
 	if t.values == nil {
-		t.values = make(map[string]float64, 10)
+		t.values = make(map[string]float64)
+		t.labeledValues = make(map[string]float64)
 	}
 
 	t.values[name] += increment
+	t.labeledValues[name+"{"+labelsString(labels)+"}"] += increment
 }
 
 // Set collects absolute value.
-func (t *TrackerMock) Set(_ context.Context, name string, absolute float64, _ ...string) {
+func (t *TrackerMock) Set(_ context.Context, name string, absolute float64, labels ...string) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
 	if t.values == nil {
-		t.values = make(map[string]float64, 10)
+		t.values = make(map[string]float64)
+		t.labeledValues = make(map[string]float64)
 	}
 
 	t.values[name] = absolute
+	t.labeledValues[name+"{"+labelsString(labels)+"}"] = absolute
 }
 
 // Value returns collected value by name.
-func (t *TrackerMock) Value(name string) float64 {
+func (t *TrackerMock) Value(name string, labels ...string) float64 {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
 	if t.values == nil {
 		return 0
+	}
+
+	if len(labels) > 0 {
+		return t.labeledValues[name+"{"+labelsString(labels)+"}"]
 	}
 
 	return t.values[name]
 }
 
 // Int returns collected value as integer by name.
-func (t *TrackerMock) Int(name string) int {
+func (t *TrackerMock) Int(name string, labels ...string) int {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
@@ -56,10 +114,14 @@ func (t *TrackerMock) Int(name string) int {
 		return 0
 	}
 
+	if len(labels) > 0 {
+		return int(t.labeledValues[name+"{"+labelsString(labels)+"}"])
+	}
+
 	return int(t.values[name])
 }
 
-// Values returns collected values as a map.
+// Values returns collected summarized values as a map.
 func (t *TrackerMock) Values() map[string]float64 {
 	t.mu.Lock()
 	defer t.mu.Unlock()
@@ -75,6 +137,43 @@ func (t *TrackerMock) Values() map[string]float64 {
 	}
 
 	return res
+}
+
+// LabeledValues returns collected labeled values as a map.
+func (t *TrackerMock) LabeledValues() map[string]float64 {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	if t.labeledValues == nil {
+		return map[string]float64{}
+	}
+
+	res := make(map[string]float64, len(t.labeledValues))
+
+	for k, v := range t.labeledValues {
+		res[k] = v
+	}
+
+	return res
+}
+
+// Metrics returns collected values in Prometheus format.
+func (t *TrackerMock) Metrics() string {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	if t.labeledValues == nil {
+		return ""
+	}
+
+	res := make([]string, 0, len(t.labeledValues))
+	for k, v := range t.labeledValues {
+		res = append(res, k+" "+strconv.FormatFloat(v, 'g', -1, 64))
+	}
+
+	sort.Strings(res)
+
+	return strings.Join(res, "\n")
 }
 
 // StatsTracker is a provider.
